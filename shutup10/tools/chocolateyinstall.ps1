0e53f5c5620bd7dfc85edd8c05e2bb8acc9e6952
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2016-10-28 18:50:49 +0200

    Added O&O ShutUp 10

---
+++ shutup10/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = 'shutup10'
+  url            = 'https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe'
+  FileFullPath   = "OOSU10.exe"
+  checksum       = '33E4560798D9CE8670A4A1082D19A78781CF4A597723A085E76594A6A8EC3440'
+  ChecksumType   = 'sha256'
+}
+
+Get-ChocolateyWebFile @packageArgs
