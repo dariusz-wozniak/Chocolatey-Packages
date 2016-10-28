17f54108820b25543feca43806a5bd3a41501cef
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2016-10-28 18:48:29 +0200

    Added Flowdock

---
+++ flowdock/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'flowdock'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://flowdock-resources.s3.amazonaws.com/windows/Install%20Flowdock.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'flowdock*'
+  checksum      = 'BAB217F64B291A74705A5071B5698219D38F5AD802ADEC3CDCF153BB9C92ECD2'
+  checksumType  = 'sha256'
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
