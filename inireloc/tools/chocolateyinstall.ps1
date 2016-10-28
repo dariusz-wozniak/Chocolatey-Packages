4f1076a8862b61712ac049ee51fcc16c1b0b7258
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2016-10-28 18:46:15 +0200

    Added inireloc

---
+++ inireloc/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'inireloc'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://ghisler.fileburst.com/addons/inireloc.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = $url
+  checksum      = '46C94CEA13E81944F02E2B1048F230C6819D2463AA624AA97DF259C827912D3E'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
