0140f1194c8fd0fbe3a8816707f22ccb5cc43314
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2016-11-05 00:45:48 +0100

    Updated shutup10 to 1.4.1384.1

--- shutup10/tools/chocolateyinstall.ps1
+++ shutup10/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName    = 'shutup10'
   url            = 'https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe'
   FileFullPath   = "OOSU10.exe"
-  checksum       = '33E4560798D9CE8670A4A1082D19A78781CF4A597723A085E76594A6A8EC3440'
+  checksum       = 'A2F6CF37E12B97F39AC70F72DF2D887EC6690E7F37C87F12D03D6F89DC08CAA8'
   ChecksumType   = 'sha256'
 }
 
