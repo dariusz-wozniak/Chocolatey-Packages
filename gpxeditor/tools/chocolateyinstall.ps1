658c2d6026c29d3b2ac64bd9ab396fab467b8d7b
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2017-09-04 21:07:44 +0200

    Updated GPX Editor to 1.6.18.1649

--- gpxeditor/tools/chocolateyinstall.ps1
+++ gpxeditor/tools/chocolateyinstall.ps1
@@ -3,14 +3,14 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'gpxeditor'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://downloads.sourceforge.net/project/gpxeditor/GPX%20Editor/Version%201.06.16/GPX_Editor_1.6.16.zip'
+$url        = 'https://netcologne.dl.sourceforge.net/project/gpxeditor/GPX%20Editor/Version%201.06.18/GPX_Editor_1_06_18.zip'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   url           = $url
   softwareName  = 'gpxeditor*'
-  checksum      = '7DE9D8C3FC90D141C2D9B50208627E298B953E004BE0C22C03982219CDEBF75D'
+  checksum      = '36FD058B0DBD36715AF519CA98A9A29CA2A634E37B7DB63185584D432B60B839'
   checksumType  = 'sha256'
 }
 
