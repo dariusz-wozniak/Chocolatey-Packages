808bfda74473cbd9c5cff44380fc794ac15fcd6d
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2017-10-03 20:51:30 +0200

    Updated Glary Utilities Pro to 5.85.0.106

--- glaryutilities-pro/tools/chocolateyinstall.ps1
+++ glaryutilities-pro/tools/chocolateyinstall.ps1
@@ -12,7 +12,7 @@ $packageArgs = @{
   validExitCodes= @(0, 3010, 1641)
   silentArgs    = '/S  /NORESTART '
   softwareName  = 'Glary Utilities'
-  checksum      = 'F5CED8E0BFE0BB7CDC2570C78B573309965EC95D3784C4CE9FAE8993D6F21FBA'
+  checksum      = 'F0569AEBC77FDBDAEB7DE414EAF4448834BB6CED0BA0EFE38465C0ACCCB138A1'
   checksumType  = 'sha256'
 }
 
