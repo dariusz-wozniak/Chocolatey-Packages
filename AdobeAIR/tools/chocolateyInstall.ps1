2b97657e2697a7eccc34263fb2c9fe4caf0140eb
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2018-11-03 13:52:15 +0100

    Update Adobe AIR to 31.0.0.96

--- AdobeAIR/tools/chocolateyInstall.ps1
+++ AdobeAIR/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
 $package = 'AdobeAIR'
-$version = '28.0'
+$version = '31.0'
 
 $params = @{
     PackageName = $package;
     FileType = 'exe';
     SilentArgs = '-silent -eulaAccepted';
     Url = "http://airdownload.adobe.com/air/win/download/$version/AdobeAIRInstaller.exe"
-    Checksum = '9076489E273652089A4A53A1D38C6631E8B7477E39426A843E0273F25BFB109F'
+    Checksum = 'DC82421F135627802B21619BDB7E4B9B0EC16D351120485C575AA6C16CD2737E'
     ChecksumType  = 'sha256'
 }
 
