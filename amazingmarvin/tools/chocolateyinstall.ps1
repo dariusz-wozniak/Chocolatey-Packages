cf092424ece3c7ec9e9913af6694e6d3266025c2
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2019-06-18 20:38:13 +0200

    Update Amazing Marvin

--- amazingmarvin/tools/chocolateyinstall.ps1
+++ amazingmarvin/tools/chocolateyinstall.ps1
@@ -1,19 +1,19 @@
 $ErrorActionPreference = 'Stop';
 
-$packageName= 'amazingmarvin'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://amazingmarvin.s3-website-us-east-1.amazonaws.com/MarvinSetup.exe'
+$packageName = 'amazingmarvin'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://amazingmarvin.s3-website-us-east-1.amazonaws.com/MarvinSetup.exe'
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url           = $url
-  softwareName  = 'amazingmarvin*'
-  checksum      = '726F324A5497361332E31E17933FD39A5E3FB0E0AD3D501AE7D91CBD6E1C27A7'
-  checksumType  = 'sha256'
-  silentArgs    = '/S'
-  validExitCodes= @(0)
+    packageName    = $packageName
+    unzipLocation  = $toolsDir
+    fileType       = 'EXE'
+    url            = $url
+    softwareName   = 'amazingmarvin*'
+    checksum       = 'D59733BABCE5786D6F49DA57BD05CBD00E9448E003B4744F20807043B3819FB3'
+    checksumType   = 'sha256'
+    silentArgs     = '/S'
+    validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
