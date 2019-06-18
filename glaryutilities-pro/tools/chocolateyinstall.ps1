510b48fd953cc1b4fe3a853e40a15b2a6e75833b
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2019-06-18 20:32:36 +0200

    Update Glary Utilities Pro

--- glaryutilities-pro/tools/chocolateyinstall.ps1
+++ glaryutilities-pro/tools/chocolateyinstall.ps1
@@ -1,19 +1,19 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName= 'glaryutilities-pro'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://download.glarysoft.com/gup5setup.exe'
+$packageName = 'glaryutilities-pro'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://download.glarysoft.com/gup5setup.exe'
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url           = $url
-  validExitCodes= @(0, 3010, 1641)
-  silentArgs    = '/S  /NORESTART '
-  softwareName  = 'Glary Utilities'
-  checksum      = 'DA406249A49BA5530469EB91819EDC96C15467AB0C86812E7FCCE517CBB3D0DE'
-  checksumType  = 'sha256'
+    packageName    = $packageName
+    unzipLocation  = $toolsDir
+    fileType       = 'EXE'
+    url            = $url
+    validExitCodes = @(0, 3010, 1641)
+    silentArgs     = '/S  /NORESTART '
+    softwareName   = 'Glary Utilities'
+    checksum       = 'CABF4A6AAF70EC1E22BC302E2175A1A23CEFE6A4BE766CB745A1AA07744D5CE5'
+    checksumType   = 'sha256'
 }
 
 Install-ChocolateyPackage @packageArgs
