dea3a1862e59ad183e5683b1df0d98774c16e90c
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2019-06-18 20:51:38 +0200

    Update Flowdock

--- flowdock/tools/chocolateyinstall.ps1
+++ flowdock/tools/chocolateyinstall.ps1
@@ -1,20 +1,20 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 
-$packageName= 'flowdock'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://flowdock-resources.s3.amazonaws.com/windows/Install%20Flowdock.exe'
+$packageName = 'flowdock'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://flowdock-resources.s3.amazonaws.com/windows/Install%20Flowdock.exe'
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url           = $url
-  softwareName  = 'flowdock*'
-  checksum      = '79C50C6D1BFDC08A0B5BAFAA65BAE73CEAF022542DF44EDC4F8F1420434529E7'
-  checksumType  = 'sha256'
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  validExitCodes= @(0)
+    packageName    = $packageName
+    unzipLocation  = $toolsDir
+    fileType       = 'EXE'
+    url            = $url
+    softwareName   = 'flowdock*'
+    checksum       = 'E1CCCF87E689DD20431587D365CF671DA8D0A2C5F62417D62A3EEE7A7B2912F9'
+    checksumType   = 'sha256'
+    silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+    validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
