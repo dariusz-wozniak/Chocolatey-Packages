7a9674f9f333515714d155758caaa3cbd0c808c3
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2019-06-18 20:51:28 +0200

    Update Kantu XModules

--- kantu-xmodules/tools/chocolateyinstall.ps1
+++ kantu-xmodules/tools/chocolateyinstall.ps1
@@ -1,19 +1,19 @@
 $ErrorActionPreference = 'Stop';
 
-$packageName= 'kantu-xmodules'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://download.a9t9.com/kantu/kantu-xmodules-setup.exe'
+$packageName = 'kantu-xmodules'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://download.ui.vision/x/ui.vision-xmodules-setup-v201905.exe'
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url           = $url
-  softwareName  = 'kantu-xmodules*'
-  checksum      = 'BAE47666E7F63F61CFAFCA3E409E0A0608965427FEB87A4FB19DF1118F68A531'
-  checksumType  = 'sha256'
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  validExitCodes= @(0)
+    packageName    = $packageName
+    unzipLocation  = $toolsDir
+    fileType       = 'EXE'
+    url            = $url
+    softwareName   = 'kantu-xmodules*'
+    checksum       = '52BDDAF246732E6BE00F78699385EFB31042ED3731784EC1BE3394E5B961EF4D'
+    checksumType   = 'sha256'
+    silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+    validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
